0c87d81d0979c51f2c96691f9a91f8d4d63e6773
Harsaroor Sohal <39523561+HarsaroorSohal@users.noreply.github.com>
2022-01-07 20:15:57 +0530

    Time: 102 ms (5.08%), Space: 5.9 MB (24.00%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int mySqrt(int x) {
         if(x == 0 || x == 1) return(x);
-        int l = 0;
+        int l = 1;
         int r = x;
         int ans = 0;
         while( l <= r)
