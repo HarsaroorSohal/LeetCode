eb6aff97f73f2945950c3b5238580ab734a1f628
Harsaroor Sohal <39523561+HarsaroorSohal@users.noreply.github.com>
2022-01-07 20:15:48 +0530

    Time: 123 ms (5.08%), Space: 5.8 MB (73.26%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 0 || x == 1) return(x);
+        int l = 0;
+        int r = x;
+        int ans = 0;
+        while( l <= r)
+        {
+            int mid = l + (r-l)/2;
+            cout << l << " " << r << " " << mid << endl;
+            if(mid == (x/mid))
+            {
+                cout << "yolo1";
+                return(mid);
+            }
+            else if(mid > x/mid)
+            {
+                cout << "yolo2";
+                r = mid - 1;
+            }
+            else
+            {
+                cout << "yolo3";
+                l = mid + 1;
+                ans = mid;
+            }
+        }
+        return(ans);
+    }
+};
